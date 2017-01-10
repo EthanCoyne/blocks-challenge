2d5e3ac53b974ccad7fe2b883cfc4705c2216eb1
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-01-10 16:29:56 -0600

    clicking box changes color to black

--- java.js
+++ java.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
      console.log('The button was clicked');
      $('body').append('<div class="box"></div>');
    });
+
+   $('body').on('click', '.box', function() {
+      console.log('The box was clicked');
+      $(this).css('background-color', 'black');
+    });
 });

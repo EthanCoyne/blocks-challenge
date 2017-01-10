655c218fa439dca03937c55d223331e2138a3787
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-01-10 17:06:59 -0600

    click 'X' to remove function complete

--- java.js
+++ java.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
 
   $('button').on('click', function() {
-     console.log('The button was clicked');
-     $('body').append('<div class="box"></div>');
-   });
+    console.log('The button was clicked');
+    $('body').append('<div class="box"><div class="x">X</div></div>');
+  });
 
-   $('body').on('click', '.box', function() {
-      console.log('The box was clicked');
-      $(this).css('background-color', 'black');
-    });
+  $('body').on('click', '.box', function() {
+    console.log('The box was clicked');
+    $(this).css('background-color', 'black');
+  });
+
+  $('body').on('click', '.x', function () {
+    console.log('x was clicked');
+    $(this).closest('.box').remove();
+  });
+
+  function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+  };
+  randomNumber(1, 9);
 });

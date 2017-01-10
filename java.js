2729fdfb018c589c07ec2900def858654a6d00e1
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-01-10 17:39:39 -0600

    completed all modes

--- java.js
+++ java.js
@@ -1,8 +1,16 @@
+
+
 $(document).ready(function() {
 
   $('button').on('click', function() {
     console.log('The button was clicked');
-    $('body').append('<div class="box"><div class="x">X</div></div>');
+    var randomColor = '#';
+
+    for (var i =0; i < 6; i++) {
+      randomColor += randomNumber(0, 9);
+    }
+    console.log(randomColor);
+    $('body').append('<div class="box" style="background-color: ' + randomColor + ';"><div class="x">X</div></div>');
   });
 
   $('body').on('click', '.box', function() {
@@ -15,8 +23,11 @@ $(document).ready(function() {
     $(this).closest('.box').remove();
   });
 
-  function randomNumber(min, max){
-    return Math.floor(Math.random() * (1 + max - min) + min);
-  };
-  randomNumber(1, 9);
+
+
+
+
 });
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+};
